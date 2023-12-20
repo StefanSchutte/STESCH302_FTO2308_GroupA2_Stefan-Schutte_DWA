24c2ec3d216d2b4090b7a351771662f7d10f5631
Stefan Schutte <2011070287@ufs4life.ac.za>
2023-12-20 21:30:40 +0900

    working on part 3

---
+++ DWA_Part_1/DWA_03_Documentation/documentation.js
@@ -0,0 +1,23 @@
+/*
+user stories
+instructions given to us.
+its unstructured
+its not grouped together
+user stories express what is required and reasoning thereof
+gives problem. built solution.
+
+for who it is.
+need expresion
+why its wanted - contextiolize
+
+
+project.awsome.org
+
+markdown file.
+
+uml
+star uml
+mermaid
+
+figma design user stories
+ */
