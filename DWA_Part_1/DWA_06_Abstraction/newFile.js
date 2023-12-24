2abb4cffa6778864dd7690d6e8013e4b6911ff51
Stefan Schutte <2011070287@ufs4life.ac.za>
2023-12-24 22:14:52 +0900

    working on 7.2

--- DWA_Part_1/DWA_06_Abstraction/newFile.js
+++ DWA_Part_1/DWA_06_Abstraction/newFile.js
@@ -3,23 +3,53 @@
 import { createUniqueId } from './exampleWithJS'
 
 /**
+ * @callback sendInvite
+ * @returns {string}
+ */
+
+/**
+ * @typedef {object} Employee
+ * @porp {string} id
+ * @porp {string} name
+ * @porp {string} company
+ * @prop {Date}} created
+ * @prop {invite} invite
+ */
+
+
+/**
  *
  * @param {string} name - legal name of employee as appearing on id.
  * @returns {Employee}
  */
-export const createEmployee = (name) => {
-    const idEmployee = createUniqueId()
+export const createEmployee = (name, company) => {
+    //const idEmployee = createUniqueId()
+    const id = new Date().getTime().toString();
 
 
     return {
         id,
         name,
+        company,
         created: new Date(),
+        invite: () => 'Not required'
     }
 }
 
+/**
+ *
+ * @param {string} name
+ * @returns {Employee}
+ */
 const createColleague = (name) => {
-    createEmployee((name, 'Codespace'))
+    const employee = createEmployee(name, 'Codespace')
+    employee.invite = () => {
+        const answer = window.prompt(`Is ${attendeeName} attending?`)
+        if (!answer || answer.trim() === '') throw new Error('Answer can not be empty')
+
+        return answer
+    };
+    return employee;
 }
 
 createEmployee('John Smith', "Woolworths")
@@ -28,24 +58,76 @@ createColleague('Schalk Venter');
 createColleague('Renzo van Wyk');
 
 //good abstraction should be composable
-const createInspector = (name) => createEmployee(name, 'gov');
-createInspector('James bond')
 
-const createEvent = ({ attendees, title }) => {
+const createStudent = (name) => {
+    const employee = creatEmployee(name, "Codespace")
+
+    employee.invite = () => {
+        return "No";
+    }
+    return employee
+}
+
+/**
+ *
+ * @param {string} name
+ * @return {Employee}
+ */
+const createInspector = (name) => {//createEmployee(name, 'gov');
+//createInspector('James bond')
+    const employee = createEmployee(name, 'gov')
+    employee.invite = () => {
+//send email
+        return "Awaiting response"
+    }
+
+    return employee;
+}
+
+/**
+ * @param {object} props
+ * @param {string} props.title
+ * @param {Array<Employee>} props.attendees
+ */
+const createEvent = (props) =>{
+
+ const { attendees, title } = props
+
+    const response ={}
+
+    for (const {name: attendeeName, invite } of attendees){
+        response[attendeeName] = invite();
+
+    // for(const { name: attendeeName, company } of attendees){
+    //     // if(company === 'Codespace') {
+    //     //     const answer = window.prompt(`Is ${attendeeName} attending?`)
+    //     //     if (!answer || answer.trim() === '') throw new Error('Answer can not be empty')
+    //     //     response[attendeeName] = answer;
+    //     //
+    //     // } else if (company === 'gov'){
+    //     //     response[attendeeName] = 'Awaiting response'
+    //     //     console.log(`Email sent to ${attendeeName}`);
+    //     // } else {
+    //     //     response[attendeeName] = 'Not required'
+    //     // }
+    }
+
     return {
         title,
         attendees,
         date: new Date(),
         completed: false,
+        response
     }
 }
 
-const event = createEvent({
+const event = eventInstance  ({
         title: 'Annual 2015 Inspection',
-    attendess: [
+    attendees: [
         createColleague('Schalk Venter'),
         createColleague("remo van wyk"),
         createInspector('mr big boss'),
+        createEmployee('jon Smith', 'wool'),
     ],
 }
 )
---
+++ DWA_Part_1/DWA_07_Programming_Paradigms/todo-example-main/modules/helper.js
@@ -0,0 +1,41 @@
+//@ts-check
+
+//Inversion of control
+
+/**
+ * ..
+ *
+ * @param {string} dataAttr
+ * @param {string} [value]
+ * @returns {HTMLElement}
+ */
+export const getHtml = (dataAttr, value) => {
+    const selector = value
+        ? `[data-${dataAttr}="${value}"]`
+        : `[data-${dataAttr}]`;
+
+    const element = document.querySelector(selector);
+    const isHtmlElement = element instanceof HTMLElement;
+
+    if (!isHtmlElement) {
+        throw new Error(`${selector} attribute not found in HTML`)
+    }
+    return element;
+}
+/**
+ * ..
+ *
+ * @param {string} dataAttr
+ * @param {string} [value]
+ * @returns {boolean}
+ */
+export const doesHtmlExist = (dataAttr, value) => {
+    const selector = value
+        ? `[data-${dataAttr}="${value}"]`
+        : `[data-${dataAttr}]`;
+
+    const element = document.querySelector(selector);
+    const isHtmlElement = element instanceof HTMLElement;
+
+    return isHtmlElement;
+}
