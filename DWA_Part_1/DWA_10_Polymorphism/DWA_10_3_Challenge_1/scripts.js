c60c091554fe628dd1b8cc7a37df62eee964b127
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-01-03 23:27:47 +0900

    working on 9.2

--- DWA_Part_1/DWA_10_Polymorphism/DWA_10_3_Challenge_1/scripts.js
+++ DWA_Part_1/DWA_10_Polymorphism/DWA_10_3_Challenge_1/scripts.js
@@ -1,86 +1,74 @@
-
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
 const STEP_AMOUNT = 1;
 
-
-
-
 const elements = {
-  number : document.querySelector('[data-key="number"]'),
-  subtract : document.querySelector('[data-key="subtract"]'),
-  add : document.querySelector('[data-key="add"]'),
-  reset : document.getElementById('resetButton'),
-  settings : document.getElementById('settingsButton'),
-}
+  number: document.querySelector('[data-key="number"]'),
+  subtract: document.querySelector('[data-key="subtract"]'),
+  add: document.querySelector('[data-key="add"]'),
+  reset: document.getElementById('resetButton'),
+  settings: document.getElementById('settingsButton'),
+  dayOption: document.getElementById('dayOption'),
+  nightOption: document.getElementById('nightOption'),
+};
+
+const drawer = document.querySelector('.drawer-overview');
+const openButton = drawer.nextElementSibling;
+const closeButton = drawer.querySelector('sl-button[variant="primary"]');
+
+openButton.addEventListener('click', () => drawer.show());
+closeButton.addEventListener('click', () => drawer.hide());
 
 const updateColor = () => {
+  const value = parseInt(elements.number.value);
+  const colorStepsAmount = 250 / (MAX_NUMBER - MIN_NUMBER);
+  const distMax = MAX_NUMBER - value;
+  const distMin = value - MIN_NUMBER;
+  const red = distMax * colorStepsAmount;
+  const green = distMin * colorStepsAmount;
+  elements.number.style.color = `rgb(${red}, ${green} , 0)`;
+};
 
-
-  const value = parseInt(elements.number.value)
-
-
-  const colorStepsAmount =250 / ( MAX_NUMBER - MIN_NUMBER )
-
-  const distMax = MAX_NUMBER - value
-  const distMin = value - MIN_NUMBER
-
-  const red = distMax * colorStepsAmount
-  const green = distMin * colorStepsAmount
-
-  elements.number.style.color = `rgb(${red}, ${green} , 0)`
-
-}
+const disableButton = (button, disable) => {
+  button.disabled = disable;
+};
 
 const subtractHandler = () => {
-  const newValue = parseInt(elements.number.value ) - STEP_AMOUNT;
+  const newValue = parseInt(elements.number.value) - STEP_AMOUNT;
   elements.number.value = newValue;
+  disableButton(elements.add, false);
+  disableButton(elements.subtract, newValue <= MIN_NUMBER);
+  updateColor();
+};
 
-  if (elements.add.disabled ){
-    elements.add.disabled = false;
-  }
-
-  if (newValue <= MIN_NUMBER){
-    elements.subtract.disabled = true;
-  }
-
-  updateColor()
-}
 const addHandler = () => {
-
-  console.log('add was clicked');
   const newValue = parseInt(elements.number.value) + STEP_AMOUNT;
   elements.number.value = newValue;
-
-  //conditional logic
-
-  if (elements.subtract.disabled){
-    elements.subtract.disabled = false;
-  }
-
-  if (newValue >= MAX_NUMBER){
-    elements.add.disabled = true;
-  }
-  updateColor()
-}
-
-elements.subtract.addEventListener('click', subtractHandler);
-elements.add.addEventListener('click', addHandler);
-
-updateColor()
+  disableButton(elements.subtract, false);
+  disableButton(elements.add, newValue >= MAX_NUMBER);
+  updateColor();
+};
 
 const resetHandler = () => {
-
   elements.number.value = 0;
-
-  // Update the color based on the new value
   updateColor();
-
-
   alert('Counter has been reset!');
 };
 
+const toggleDayNightMode = (isNightMode) => {
+  document.documentElement.classList.toggle('sl-theme-dark', isNightMode);
+};
+
+elements.subtract.addEventListener('click', subtractHandler);
+elements.add.addEventListener('click', addHandler);
 elements.reset.addEventListener('click', resetHandler);
 
+elements.dayOption.addEventListener('click', () => toggleDayNightMode(false));
+elements.nightOption.addEventListener('click', () => toggleDayNightMode(true));
+
+// Set initial theme based on the system preference
+const isNightModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
+toggleDayNightMode(isNightModePreferred);
 
 
+updateColor();
