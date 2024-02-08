a24f3b93c531d34fe4129c85aa9cf2f38c07fe1d
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-02-09 00:08:40 +0200

    working_on_10.1

--- DWA_Part_1/DWA_10_Polymorphism/DWA_10_3_Challenge_1/scripts.js
+++ DWA_Part_1/DWA_10_Polymorphism/DWA_10_3_Challenge_1/scripts.js
@@ -64,8 +64,6 @@ class InteractiveCounter {
     this.closeButton = this.drawer.querySelector('sl-button[variant="primary"]');
 
     this.setupEventListeners();
-    this.setInitialTheme();
-    this.updateColor();
   }
 
   /**
@@ -81,18 +79,6 @@ class InteractiveCounter {
     this.elements.nightOption.addEventListener('click', () => this.toggleDayNightMode(true));
   }
 
-  /**
-   * Updates the text color of the counter based on its value.
-   */
-  updateColor() {
-    const value = parseInt(this.elements.number.value);
-    const colorStepsAmount = 250 / (this.MAX_NUMBER - this.MIN_NUMBER);
-    const distMax = this.MAX_NUMBER - value;
-    const distMin = value - this.MIN_NUMBER;
-    const red = distMax * colorStepsAmount;
-    const green = distMin * colorStepsAmount;
-    this.elements.number.style.color = `rgb(${red}, ${green} , 0)`;
-  }
 
   /**
    * Disables or enables a button.
@@ -111,7 +97,6 @@ class InteractiveCounter {
     this.elements.number.value = newValue;
     this.disableButton(this.elements.add, false);
     this.disableButton(this.elements.subtract, newValue <= this.MIN_NUMBER);
-    this.updateColor();
   }
 
   /**
@@ -122,7 +107,6 @@ class InteractiveCounter {
     this.elements.number.value = newValue;
     this.disableButton(this.elements.subtract, false);
     this.disableButton(this.elements.add, newValue >= this.MAX_NUMBER);
-    this.updateColor();
   }
 
   /**
@@ -130,26 +114,19 @@ class InteractiveCounter {
    */
   resetHandler() {
     this.elements.number.value = 0;
-    this.updateColor();
+
     alert('Counter has been reset!');
   }
 
-  /**
-   * Toggles the day or night mode.
-   * @param {boolean} isNightMode - Whether to enable night mode.
-   */
   toggleDayNightMode(isNightMode) {
-    document.documentElement.classList.toggle('sl-theme-dark', isNightMode);
-  }
-
-  /**
-   * Sets the initial theme based on the system's color scheme preference.
-   */
-  setInitialTheme() {
-    const isNightModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    this.toggleDayNightMode(isNightModePreferred);
+    if (isNightMode) {
+      document.documentElement.classList.add('sl-theme-dark');
+      document.documentElement.classList.remove('sl-theme-light');
+    } else {
+      document.documentElement.classList.remove('sl-theme-dark');
+      document.documentElement.classList.add('sl-theme-light');
+    }
   }
 }
 
-// Create an instance of the InteractiveCounter class
 const interactiveCounter = new InteractiveCounter();
